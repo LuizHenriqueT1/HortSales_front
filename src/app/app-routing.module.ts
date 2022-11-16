b16648a4dc264dd5d657728014fb85b83f8914b1
LuizHenriqueT1 <luizhenrique201387@gmail.com>
2022-11-16 17:48:42 -0300

    Feito guard para rotas, logout, mudança de prod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HeaderComponent } from './core/components/header/header.component';
+import { AuthGuardGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   {
-    path: '', component: HeaderComponent,
+    path: '', component: HeaderComponent, canActivate: [AuthGuardGuard]
   },
 ];
 
--- src/app/funcionarios/funcionarios-routing.module.ts
+++ src/app/funcionarios/funcionarios-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from '../shared/guard/auth.guard';
 import { FuncionarioCreateComponent } from './funcionario-create/funcionario-create/funcionario-create.component';
 import { FuncionarioDeleteComponent } from './funcionario-delete/funcionario-delete/funcionario-delete.component';
 import { FuncionarioDetailsComponent } from './funcionario-details/funcionario-details/funcionario-details.component';
@@ -7,16 +8,35 @@ import { FuncionarioUpdateComponent } from './funcionario-update/funcionario-upd
 import { FuncionariosComponent } from './funcionarios.component';
 
 const routes: Routes = [
-  { path: 'funcionarios', component: FuncionariosComponent },
-  { path: 'create-funcionario', component: FuncionarioCreateComponent },
-  { path: 'editar-funcionario/:id', component: FuncionarioUpdateComponent },
-  { path: 'detalhes/:id', component: FuncionarioDetailsComponent },
-  { path: 'deletar/:id', component: FuncionarioDeleteComponent }
+  {
+    path: 'funcionarios',
+    component: FuncionariosComponent,
+    canActivate: [AuthGuardGuard],
+  },
+  {
+    path: 'create-funcionario',
+    component: FuncionarioCreateComponent,
+    canActivate: [AuthGuardGuard],
+  },
+  {
+    path: 'editar-funcionario/:id',
+    component: FuncionarioUpdateComponent,
+    canActivate: [AuthGuardGuard],
+  },
+  {
+    path: 'detalhes/:id',
+    component: FuncionarioDetailsComponent,
+    canActivate: [AuthGuardGuard],
+  },
+  {
+    path: 'deletar/:id',
+    component: FuncionarioDeleteComponent,
+    canActivate: [AuthGuardGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FuncionariosRoutingModule { }
- 
+export class FuncionariosRoutingModule {}
